f583abb7da243549dbacb3f6b4a8400dae4a4b37
C-ON <vmfpsel@gmail.com>
2025-01-25 14:50:59 +0900

    public path update

--- Vue/vue-blog/vue.config.js
+++ Vue/vue-blog/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/ci5n0222.github.io/'
 })
