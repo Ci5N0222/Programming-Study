b17acfce755250f64c9277cf81e6778425519258
Sion-Noh <vmfpsel@gmail.com>
2025-01-30 22:16:38 +0900

    styled-components study

--- react/mini-mall01/README.md
+++ react/mini-mall01/README.md
@@ -6,6 +6,7 @@
 
 #### 2. React-Router
 ```bash
+/** Install **/
 $ npm install react-router-dom
 
 // 1. main.tsx에 BrowserRouter Import
@@ -20,4 +21,33 @@ import { BrowserRouter } from 'react-router-dom'
  <Routes>
     <Route path="/" element={ <Home /> } />
  </Routes>
+```
+
+#### 3. Styled-Components
+```bash
+/** Install **/
+$ npm install styled-components
+
+// 1. 사용할 컴포넌트에 Import
+import styled from "styled-components"
+
+// 2. 사용법
+type YellowBtnType = {
+  bg :string
+  color? :string
+}
+
+// 변수를 받을 수 있으며, 삼항연산자도 사용 가능
+let YellowBtn = styled.button<YellowBtnType>`
+  background: ${ props => props.bg };
+  color: ${ props => props.color == undefined ? 'black' : 'red'};
+  padding: 10px;
+`
+
+// 같은 버튼을 복제하여 확장할 수 있다.
+let NewBtn = styled(YellowBtn)`
+  border: 1px solid black;
+  font-weight: bold;
+`
+`
 ```
--- react/mini-mall01/src/components/Detail.tsx
+++ react/mini-mall01/src/components/Detail.tsx
@@ -3,6 +3,24 @@ import { addComma } from "../assets/common/common"
 import { shoesType, shoesData } from "../assets/data/shoes"
 import { useParams } from "react-router-dom"
 import { Error } from "../pages/Error/Error"
+import styled from "styled-components"
+
+type YellowBtnType = {
+  bg :string
+  color? :string
+}
+
+// Styled-Components
+let YellowBtn = styled.button<YellowBtnType>`
+  background: ${ props => props.bg };
+  color: ${ props => props.color == undefined ? 'black' : 'red'};
+  padding: 10px;
+`
+
+let NewBtn = styled(YellowBtn)`
+  border: 1px solid black;
+  font-weight: bold;
+`
 
 export const Detail = () => {
   // useParams : 현재 url의 파리미터를 받아온다.
@@ -21,6 +39,8 @@ export const Detail = () => {
   
   return (
     <div className="container">
+      <YellowBtn bg='blue'>버튼</YellowBtn>
+      <NewBtn bg='white'>버튼</NewBtn>
       <div className="row">
         <div className="col-md-6">
           <img src={product.image} width="100%" />
