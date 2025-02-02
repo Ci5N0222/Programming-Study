6462e697e648b2982ce9c1125dda7b3ef506bf26
C-ON <vmfpsel@gmail.com>
2025-02-02 14:37:24 +0900

    node-react study

--- node/study01/README.md
+++ node/study01/README.md
@@ -6,3 +6,20 @@
 
 3. npm install express
 
+4. start => $ node server.js
+
+### Create React Project
+``` bash
+// Create New Project
+$ npm create vite@latest "my project name" --template react-ts
+
+// Select
+1. React
+2. TypeScript
+
+// Start
+1. $ cd "my project name"
+2. $ npm install
+3. $ npm run build
+```
+```
--- node/study01/server.js
+++ node/study01/server.js
@@ -5,3 +5,26 @@ const path = require('path')
 app.listen(8080, () => {
     console.log('node server start');
 });
+
+// 해당 파일을 사용하기 위해 등록
+app.use(express.static(path.join(__dirname, 'node-react01/dist')));
+
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'node-react01/dist/index.html'));
+});
+
+// DB에서 데이터를 받아 response 하는 경우
+app.get('/product', (req, res) => {
+    // DB에서 데이터 조회했다고 가정
+    const result = { name : "jack" }
+    res.join(result);
+});
+
+// React-Router를 사용하는 경우 작성
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'node-react01/dist/index.html'));
+});
