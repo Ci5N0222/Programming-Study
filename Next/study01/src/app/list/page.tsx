2ae8e6e3b10ee60af8034da0e7ed83a471c79479
Sion-Noh <vmfpsel@gmail.com>
2025-02-02 23:23:20 +0900

    게시물 반복문으로 만들기기

--- Next/study01/src/app/list/page.tsx
+++ Next/study01/src/app/list/page.tsx
@@ -1,15 +1,30 @@
 import styles from './page.module.css';
 
-export default function List(){
-    return (
-        <div>
-            <h2 className={ styles.title }>Products</h2>
-            <div className={ styles.food }>
-                <h4>상품명 $40</h4>
+export default function List() {
+
+  let products = [
+    { title : "Tomatoes", price : 3 },
+    { title : "Pasta", price : 5 },
+    { title : "Coconut", price : 5 }
+  ];
+
+  return (
+    <div>
+      <h2 className={styles.title}>Products</h2>
+      {
+        products.map(item => {
+          return (
+            <div className={styles.food}>
+              <div className={ styles.image }>
+              </div>
+              <div className={ styles.info }>
+                <h4>{ item.title } </h4>
+                <p>${ item.price }</p>
+              </div>
             </div>
-            <div className={ styles.food }>
-                <h4>상품명 $40</h4>
-            </div>
-        </div>
-    )
+          )
+        })
+      }
+    </div>
+  )
 }
