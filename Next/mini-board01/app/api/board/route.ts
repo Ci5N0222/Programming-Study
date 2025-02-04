f02c90c000c23de7f26b300c0164df763cdaaf94
Sion-Noh <vmfpsel@gmail.com>
2025-02-05 00:37:56 +0900

    간단한 글 작성기능 study

--- Next/mini-board01/app/api/board/route.ts
+++ Next/mini-board01/app/api/board/route.ts
@@ -1,9 +1,8 @@
-import { connectDB, getConnect } from "@/util/database";
+import { getConnect } from "@/util/database";
 import { NextRequest, NextResponse } from "next/server";
 
 // DB Connect
 const db = await getConnect("forum", "post");
-
 
 // GET 요청 핸들러
 export async function GET(req: NextRequest) {
@@ -13,15 +12,22 @@ export async function GET(req: NextRequest) {
 // POST 요청 핸들러
 export async function POST(req: NextRequest) {
 
-    // Object 형태로 데이터를 보냈을 때
-    // const body = req.json();
-
-     // form 태그를 사용하여 데이터를 보냈을 때
+    // form 태그를 사용하여 데이터를 보냈을 때
     const formData = await req.formData();
     const title = formData.get("title");
     const content = formData.get("content");
 
-    db.insertOne({ title, content });
+    if(title === "") return NextResponse.json("제목을 입력하세요.");
+    if(content === "") return NextResponse.json("내용을 입력하세요.");
 
-    return NextResponse.json("POST", { status: 200 });
+    try {
+        // insertOne : MongoDB document insert function
+        db.insertOne({ title, content });
+        return NextResponse.redirect("/board", { status: 200 });
+    } catch(err) {
+        return NextResponse.json("오류가 발생했습니다.", { status: 500 });
+    }
+    
+
+    
 }
